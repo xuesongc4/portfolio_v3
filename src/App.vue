eb242cbb69081fcff2f6192a7a5cfa451da94f71
jchen <jchen@goldencomm.com>
2019-09-20 00:14:42 -0700

    added twinkling stars to intro page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <Menu :menuItems="pages"/>
+    <MainMenu :menuItems="pages"/>
     <Intro/>
   </div>
 </template>
 
 <script>
 import Intro from './views/Intro'
-import Menu from './components/Menu'
+import MainMenu from './components/MainMenu'
 
 export default {
   name: 'app',
   components: {
     Intro,
-    Menu
+    MainMenu
   },
   data(){
     return {
@@ -36,5 +36,26 @@ export default {
 body{
   margin:0;
   overflow-x: hidden;
+}
+
+@keyframes twinkle {
+  0% {
+    transform: scale(1, 1);
+    background: rgba(0,0,0,0.0);
+    animation-timing-function: ease-in;
+  }
+  60% {
+    transform: scale(0.8, 0.8);
+    background: rgba(0,0,0,1);
+    animation-timing-function: ease-out;
+  }
+  80% {
+    background: rgba(0,0,0,0.00);
+    transform: scale(1, 1);
+  }
+  100% {
+    background: rgba(0,0,0,0.0);
+    transform: scale(1, 1);
+  }
 }
 </style>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,37 +1,71 @@
 <template>
     <div id="intro">
         <div class="intro-content">
-            <p class="item-1">Hi there! My name is Jason and I'm a web developer who is based out of Orange County CA.</p>
+            <p class="item-1">Hi there! My name is Jason and I'm a web developer who is based out of Orange County
+                CA.</p>
             <p class="item-2">I enjoy hanging out with my friends and family, sports, coding and...</p>
-            <p class="item-3">day dreaming...</p>
+            <p class="item-3">looking up at the stars...</p>
         </div>
         <div class="intro-title">
             <h1>Jason Chen</h1>
             <h3>&lt;web_developer&gt;</h3>
         </div>
+        <div v-for="(star, $index) in stars" :style="star" :key="$index" class="star" ></div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Intro',
-        props: {
-            msg: String
-        }
+        data: function(){
+            return{
+                stars:[]
+            }
+        },
+        methods: {
+            twinkleStarLoader() {
+                const numberOfStars = 100;
+                for (let i = 0; i < numberOfStars; i++) {
+                    const twinkle = 'twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5)+2)+'s infinite';
+                    const topPx = Math.random()*window.innerHeight+'px';
+                    const leftPx = Math.random()*window.innerWidth+'px';
+                    const star = {
+                        animation: twinkle,
+                        top: topPx,
+                        left: leftPx
+                    }
+                    this.stars[i] = star;
+                }
+            }
+        },
+        beforeMount(){
+            this.twinkleStarLoader()
+        },
     }
 </script>
 <style scoped>
-    #intro{
+    #intro {
         height: 100vh;
         width: 100vw;
-        background: url('../assets/stars.png')center center no-repeat;
+        background: url('../assets/stars.png') center center no-repeat;
         background-size: cover;
         position: relative;
     }
-    .intro-content{
+
+    .intro-content {
         position: absolute;
     }
-    .intro-title{
+
+    .intro-title {
         position: absolute;
     }
+
+    .star {
+        position: absolute;
+        width: 4px;
+        height: 4px;
+        background: rgba(0,0,0,0);
+        border-radius: 5px;
+    }
+
 </style>
