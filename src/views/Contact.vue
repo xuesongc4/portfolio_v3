8bb350aed7a3994f6cbfe3fbd2d5f965da061178
xuesongc <jchen@goldencomm.com>
2019-10-25 01:06:38 -0700

    mistake

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -95,7 +95,7 @@
                     fetch('/', {
                         method: 'post',
                         headers: {
-                            'Content-Type': 'application/x-www-urlencoded'
+                            'Content-Type': 'application/x-www-form-urlencoded'
                         },
                         body: this.encode({
                             'form-name': 'contact',
